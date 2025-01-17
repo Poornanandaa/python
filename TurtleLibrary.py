c4de57bfac72ac06a9d7dd09d45d8f259a05d4d5
Poornanand Arun mohan <1074743@lammersvilleusd.net>
2025-01-16 19:37:43 -0800

    Done!

---
+++ TurtleLibrary.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("Green")
+screen=turtle.Screen()
+screen.setup(400,300)
+turtle.title("New York Library")
+pen=turtle.Turtle()
+for i in range(4):
+    pen.forward(100)
+    pen.left(90)
+    i=i+1
+turtle.done()
