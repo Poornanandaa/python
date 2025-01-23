5c3548d6bd6623a5f4805b263cafea096f25c3c6
Poornanand Arun mohan <1074743@lammersvilleusd.net>
2025-01-22 19:07:37 -0800

    Done!

---
+++ TurtleLibrary2.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("Blue")
+screen=turtle.Screen()
+screen.setup(400,300)
+turtle.title("Los Angeles Library")
+pen=turtle.Turtle()
+for i in range(8):
+    pen.forward(100)
+    pen.left(45)
+i=i+1
+turtle.done() 
