a21026f32e8b93d1b2b5dc8b6f0288ba679b6457
Poornanandaa <1074743@lammersvilleusd.net>
2025-02-27 19:44:52 -0800

    Add files via upload

---
+++ Library.py
@@ -0,0 +1,11 @@
+class library:
+    def __init__(self,list,name):
+        self.list=list
+        self.name=name
+        self.lend={}
+class list:
+    print("Welcome to the Local Library")
+    print("What book would you like?")        
+    
+
+        
